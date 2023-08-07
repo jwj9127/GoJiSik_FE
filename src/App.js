5218aad2793732f3b15ddbc59f6329c0e752e89c
조아빈 <whdkqls122@naver.com>
2023-08-07 19:11:51 +0900

    아직 한참

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import Navigation from './components/Navigation'
 import {Routes, Route} from 'react-router-dom';
 import Goverment from './components/Goverment'
+import Postpage from './pages/Postpages';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Routes>
+        <Route path='/goverment24' element={<Goverment/>}/>
+        <Route path='/post' element={<Postpage/>} />
+      </Routes>
     </div>
   );
 }
