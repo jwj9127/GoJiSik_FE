c7baf5adb64dc9e79d10c80b28326daca6f61859
조아빈 <whdkqls122@naver.com>
2023-08-13 22:47:14 +0900

    css 하는 중

--- src/App.js
+++ src/App.js
@@ -32,35 +32,36 @@ function App() {
   return (
     <div className="App">
       <Navigation/>
-      <Routes>
-        <Route path='/goverment24' element={<Goverment/>}/>
-        <Route path='/post' element={<Postpages/>} />
-        <Route path='/main' element={<Main/>}/>
-        <Route path='/post/postWrite' element={<PostWritepage/>}/>  
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/postpage' element={<Postpages/>}/>
-        <Route path='/joinpage' element={<Joinpage/>}/>
-        <Route path='/movie' element={<MoviePage/>}/>
-        <Route path='/cgv' element={<Cgv/>}/>
-        <Route path='/cafe' element={<CafePage/>}/>
-        <Route path='/food' element={<FoodPage/>}/>
-        <Route path='/dessert' element={<DessertPage/>}/>
-        <Route path='/eCormmerce' element={<Ecommerce/>}/>
-        <Route path="/myPage" element = {<MyPage />}></Route>
-        <Route path="/postHistoryPage" element = {<PostHistoryPage />}></Route>
-        <Route path="/answerHistoryPage" element = {<AnswerHistoryPage />}></Route>
-        <Route path="/informationPage" element = {<InformationPage />}></Route>
-        <Route path="/withdrawal" element = {<Withdrawal />}></Route>
-        <Route path="/JuminPage" element = {<JuminPage />}></Route>
-        <Route path="/Year" element = {<Year />}></Route>
-        <Route path="/Family" element = {<Family />}></Route>
-        <Route path="/Movie" element = {<Movie />}></Route>
-        <Route path="/Cafe" element = {<Cafe />}></Route>
-        <Route path="/Food" element = {<Food />}></Route>
-        <Route path="/Dessert" element = {<Dessert />}></Route>
-        <Route path="/Cgv" element = {<Cgv />}></Route>
-        <Route path='/Agreepage' element={<Agreepage/>}/>
-      </Routes>
+      <div className='App-body'>
+        <Routes>
+          <Route path='/' element={<Main/>}/>
+          <Route path='/goverment24' element={<Goverment/>}/>
+          <Route path='/post' element={<Postpages/>} />
+          <Route path='/post/postWrite' element={<PostWritepage/>}/>  
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/joinpage' element={<Joinpage/>}/>
+          <Route path='/movie' element={<MoviePage/>}/>
+          <Route path='/cgv' element={<Cgv/>}/>
+          <Route path='/cafe' element={<CafePage/>}/>
+          <Route path='/food' element={<FoodPage/>}/>
+          <Route path='/dessert' element={<DessertPage/>}/>
+          <Route path='/eCormmerce' element={<Ecommerce/>}/>
+          <Route path="/myPage" element = {<MyPage />}></Route>
+          <Route path="/postHistoryPage" element = {<PostHistoryPage />}></Route>
+          <Route path="/answerHistoryPage" element = {<AnswerHistoryPage />}></Route>
+          <Route path="/informationPage" element = {<InformationPage />}></Route>
+          <Route path="/withdrawal" element = {<Withdrawal />}></Route>
+          <Route path="/JuminPage" element = {<JuminPage />}></Route>
+          <Route path="/Year" element = {<Year />}></Route>
+          <Route path="/Family" element = {<Family />}></Route>
+          <Route path="/Movie" element = {<Movie />}></Route>
+          <Route path="/Cafe" element = {<Cafe />}></Route>
+          <Route path="/Food" element = {<Food />}></Route>
+          <Route path="/Dessert" element = {<Dessert />}></Route>
+          <Route path="/Cgv" element = {<Cgv />}></Route>
+          <Route path='/Agreepage' element={<Agreepage/>}/>
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState  } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function Postpages() {
-    const [value,setValue] = useState(true);
+    const [value, setValue] = useState(true);
+    const [postlist, setPostlist] = useState(null);
     const navigate = useNavigate();
 
-    // 이따 보기!!!
-    // useEffect(() => {
-    //     axios({
-    //         method : 'get',
-    //         url : 
-    //     })
+    // const getpostlist = async() => {
+    //     const response = await (await axios.get('//localhost:8080/questions')).data
+    //     setPostlist(response.data)
+    // }
+
+    // useEffect(()=>{
+    //     getpostlist();
     // }, [])
 
     const toggleclick = e => {  
@@ -22,6 +24,14 @@ export default function Postpages() {
     }
     return(
         <div className="postbox">
+            <h1>게시글 목록</h1>
+            <ul className="postlist">
+                {/* {postlist.map((post)=> (
+                    <li key={post.id}>
+                        <Link to={`/post/${post.id}`}>{post.title}</Link>
+                    </li>
+                ))} */}
+            </ul>
             <button className="btn postbox-viewtogglebtn" onClick={toggleclick}>{value?'조회순':'최신순'}</button>
             <button className="btn postbox-postwritebox" onClick={MoveToWrite}>게시글 작성</button>
         </div>
