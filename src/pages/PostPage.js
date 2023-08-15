724d9973dcf8f890f89f54dbfb88d460d6a4c86a
조아빈 <whdkqls122@naver.com>
2023-08-16 00:24:46 +0900

    ww

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState  } from "react";
-import {useNavigate } from "react-router-dom";
+import {useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import '../css/postpages.css'
+
 
 export default function Postpages() {
     const [value, setValue] = useState(true);
