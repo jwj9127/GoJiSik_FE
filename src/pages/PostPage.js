6b361666f54c2c76bb4def02ea0e32a68e7f2313
조아빈 <whdkqls122@naver.com>
2023-08-16 01:10:33 +0900

    제발

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -50,7 +50,7 @@ export default function Postpages() {
             </div>
             <div className="postbox-postlistbox">
             <ul className="postlist">
-                {postlist?.map((post) => (
+                {postlist.data?.map((post) => (
                     <li key={post.id}>
                          <Link to={`/post/${post.id}`}>{post.title}</Link>
                          작성자: {post.name}
