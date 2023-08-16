6bcb952c63bffb00c5cfa44d92522b0263daa7fe
정희수 <gmltn3780@naver.com>
2023-08-16 22:14:52 +0900

    로그인 중간수정

--- src/pages/Kiosk/KioskPage.js
+++ src/pages/Kiosk/KioskPage.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import '../../css/kiosk/KioskPage.css';
 
 function KioskPage() {
+    const navigate = useNavigate();
     return (
         <>
             <div className="title">
