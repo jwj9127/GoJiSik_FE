907fe8990cedf31e313c14ce3be408f1cbbe6410
조아빈 <whdkqls122@naver.com>
2023-08-16 19:48:35 +0900

    ㅋㅋ

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -12,54 +12,61 @@ export default function MyPage() {
     const backgroundArr = [img1, img2, img3, img4, img5];
     const randomIndex = Math.floor(Math.random() * backgroundArr.length);
     const backgroundImg = backgroundArr[randomIndex];
-    const [token, setToken] = useState();
-    useEffect(()=>{
-        setToken(window.localStorage.getItem('token'));
-    })
-
+    
     const handleDeleteProfile = (e) => {
         e.preventDefault();
         if (window.confirm('확인을 누르면 회원 정보가 삭제됩니다.')) {
-          axios
+            axios
             .delete('//localhost:8080/users/{id}')
             .then(() => {
-              alert('그동안 이용해주셔서 감사합니다.');
+                alert('그동안 이용해주셔서 감사합니다.');
             })
             .then(
                 ()=>{
                     navigator('/');
                 }
-            )
-            .catch((err) => alert(err.response.data.message));
-        } else {
-          return;
-        }
-    };
-
+                )
+                .catch((err) => alert(err.response.data.message));
+            } else {
+                return;
+            }
+        };
+        
     const [id, setId] = useState([]);
-    const [name, setName] = useState([]);
     const [grade, setGrade] = useState([]);
     const [point, setPoint] = useState([]);
+    const [token, setToken] = useState();
+    
     useEffect(() => {
-        axios
-          .get("//localhost:8080/users/{id}")
-          .then((response) => {
-            setId(response.data);
-            setName(response.data);
-            // setGrade(response.data);
-            // setPoint(response.data);
-          });
-      }, []);
+        const storedId = window.localStorage.getItem("phonenum");
+        const storedToken = window.localStorage.getItem("token");
 
-
-
+        if (storedId && storedToken) {
+            setId(storedId);
+            setToken(storedToken);
+      
+            axios
+              .get(`//localhost:8080/users/${storedId}`, {
+                headers: { Authorization: `Bearer ${storedToken}` },
+              })
+              .then((result) => {
+                window.localStorage.setItem("phonenum", result.data.phonenum);
+                // setGrade(response.data);
+                // setPoint(response.data);
+              })
+              .catch((err) => alert(err.response.data.message));
+          }
+        }, []);
+    
+    
+    
     return (
         <>
             <div className='mypage_main'>
                 <div className='profil' style={{ backgroundImage: `url(${backgroundImg})` }}>{id}</div>
 
                 <div className='detail'>
-                    <div className='nickname'>{name}</div>
+                    <div className='nickname'>{id}</div>
                     <div className='gradeAndpoint'>
                         {/* <div className='grade'>{grade}</div>
                         <div className='point'>{point}</div> */}
